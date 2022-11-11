659e8498397f876525ebecabc0a4707ac11a47ab
Cifre0 <99344829+cifre0@users.noreply.github.com>
2022-11-11 03:14:15 +0100

    Update scriptCrontab.sh

--- statuscake/scriptCrontab.sh
+++ statuscake/scriptCrontab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if [[ $(systemctl is-active nginx) == "active" ]]; then curl -X POST "https://push.statuscake.com/?PK=53c95866914c6cf&TestID=6678558&time=0" ; fi
+if [[ $(service nginx status | awk '{print $4}') == "running" ]]; then curl -X POST "https://push.statuscake.com/?PK=53c95866914c6cf&TestID=6678558&time=0" ; fi
